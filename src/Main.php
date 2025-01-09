45fcda652da768c840fe59763c575435d3770819
lubro0 <142106680+lubro0@users.noreply.github.com>
2025-01-09 18:12:06 +0100

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace lubro\Navigator;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
+use jojoe77777\FormAPI\CustomForm;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $compass = VanillaItems::COMPASS();
+        $compass->setCustomName("§eNavigator");
+        $player->getInventory()->setItem(4, $compass);
+    }
+
+    public function onPlayerDropItem(PlayerDropItemEvent $event): void {
+        $item = $event->getItem();
+        if ($item->getCustomName() === "§eNavigator") {
+            $event->cancel();
+        }
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        if ($item->getCustomName() === "§eNavigator") {
+            $form = new CustomForm(function (Player $player, $data) {});
+            $form->setTitle("Navigator");
+            $form->addLabel("SOON");
+            $player->sendForm($form);
+        }
+    }
+}
