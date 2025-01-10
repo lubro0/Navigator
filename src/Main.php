a7ede556773c1b175745f5575313d1cb28af9d87
lokiPM <142106680+lokiPM@users.noreply.github.com>
2025-01-10 20:15:15 +0100

    Using old Code because im cool!

--- src/Main.php
+++ src/Main.php
@@ -6,42 +6,45 @@ namespace lobbycraft\LobbyPM;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
-use pocketmine\item\Item;
 use pocketmine\player\Player;
+use jojoe77777\FormAPI\CustomForm;
 
 class Main extends PluginBase implements Listener {
 
-    private array $got_items = [];
-
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        if (!$this->getServer()->getPluginManager()->getPlugin("FormAPI")) {
+            $this->getLogger()->warning("FormAPI not found!");
+        }
     }
 
     public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
-        $this->got_items[$player->getName()] = false;
-    }
-
-    public function onPlayerMove(PlayerMoveEvent $event): void {
-        $player = $event->getPlayer();
-        if (!$this->got_items[$player->getName()]) {
-            $item = ItemFactory::getInstance()->get(ItemIds::DYE, 6); // Cyan Dye
-            $item->setCustomName("§9Settings");
-            $player->getInventory()->setItem(4, $item);
-
-            $this->got_items[$player->getName()] = true;
-        }
+        $item = ItemFactory::getInstance()->get(ItemIds::DYE, 6); // Cyan Dye
+        $item->setCustomName("§9Settings");
+        $player->getInventory()->setItem(4, $item);
     }
 
     public function onPlayerDropItem(PlayerDropItemEvent $event): void {
         $item = $event->getItem();
         if ($item->getCustomName() === "§9Settings") {
             $event->cancel();
+        }
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event): void {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        if ($item->getCustomName() === "§9Settings") {
+            $form = new CustomForm(function (Player $player, $data) {});
+            $form->setTitle("Settings");
+            $form->addLabel("SOON");
+            $player->sendForm($form);
         }
     }
 }
