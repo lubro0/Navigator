752f557383f13b8f6641c5d6c0c79363eb778b71
lubro0 <142106680+lubro0@users.noreply.github.com>
2025-01-10 15:34:29 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -2,22 +2,17 @@
 
 declare(strict_types=1);
 
-namespace lubro\Navigator;
+namespace lobbycraft\LobbyPM;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerDropItemEvent;
-use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\item\VanillaItems;
-use pocketmine\player\Player;
-use jojoe77777\FormAPI\CustomForm;
-use jojoe77777\FormAPI\SimpleForm;
 
 class Main extends PluginBase implements Listener {
 
-    private array $compass_get = [];
+    private array $got_items = [];
 
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -25,40 +20,16 @@ class Main extends PluginBase implements Listener {
 
     public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
-        $this->compass_get[$player->getName()] = false;
-        $player->setAllowFlight(true);
+        $this->got_items[$player->getName()] = false;
     }
 
     public function onPlayerMove(PlayerMoveEvent $event): void {
         $player = $event->getPlayer();
-        if ($player->getLocation() !== $event->getFrom() && !$this->compass_get[$player->getName()]) {
-            $compass = VanillaItems::COMPASS();
-            $compass->setCustomName("§eNavigator");
-            $player->getInventory()->setItem(4, $compass);
-            $this->compass_get[$player->getName()] = true;
-            $player->setAllowFlight(false);
-        }
-    }
-
-    public function onPlayerDropItem(PlayerDropItemEvent $event): void {
-        $item = $event->getItem();
-        if ($item->getCustomName() === "§eNavigator") {
-            $event->cancel();
-        }
-    }
-
-    public function onPlayerInteract(PlayerInteractEvent $event): void {
-        $player = $event->getPlayer();
-        $item = $event->getItem();
-        if ($item->getCustomName() === "§eNavigator") {
-            $form = new SimpleForm(function (Player $player, $data) {
-                if ($data === 0) {
-                    $player->sendMessage("Soon!");
-                }
-            });
-            $form->setTitle("Navigator");
-            $form->addButton("SMP");
-            $player->sendForm($form);
+        if (!$this->got_items[$player->getName()]) {
+            $rod = VanillaItems::BLAZE_ROD();
+            $rod->setCustomName("§9Ranks");
+            $player->getInventory()->setItem(3, $rod);
+            $this->got_items[$player->getName()] = true;
         }
     }
 }
